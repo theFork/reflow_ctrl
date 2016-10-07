cd76982026ab6a60cb15fa04620bf9f8b6e5e5fb
Simon Gansen <simon@sineband.de>
2016-10-07 17:57:39 +0200

    Stub control runnable

---
+++ software/host/src/de/umidi/reflowcontrol/controller/ControlRunnable.java
@@ -0,0 +1,13 @@
+package de.umidi.reflowcontrol.controller;
+
+public class ControlRunnable implements Runnable {
+
+    /**
+     * Executed by the controller in fixed intervals (1s)
+     */
+    @Override
+    public void run() {
+        // TODO
+        System.out.println("hi");
+    }
+}
--- software/host/src/de/umidi/reflowcontrol/controller/ReflowController.java
+++ software/host/src/de/umidi/reflowcontrol/controller/ReflowController.java
@@ -1,8 +1,56 @@
 package de.umidi.reflowcontrol.controller;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import de.umidi.reflowcontrol.model.ReflowModel;
+import de.umidi.reflowcontrol.view.ReflowView;
+
+/**
+ * Main controller class
+ */
 public class ReflowController {
-    // TODO:
-    // - Setup
+    // This class handles:
+    // - Main function and setup
     // - Runnable for fixed-interval execution
     // - Tear down
+
+    private final int CONTROL_INTERVAL_MILLIS = 1000;
+
+    ReflowView view = new ReflowView();
+    ReflowModel model = new ReflowModel();
+    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    public static void main(String args[]) {
+
+        ReflowController reflowController = new ReflowController();
+
+        // Setup view
+        reflowController.view.setVisible(true);
+
+        // Setup model
+
+        // Run button
+        // TODO
+        reflowController.startButtonAction();
+
+        // Stop button
+
+        // Quit button
+    }
+
+    private void startButtonAction() {
+        ControlRunnable r = new ControlRunnable();
+        executor.scheduleAtFixedRate(r, 0, CONTROL_INTERVAL_MILLIS, TimeUnit.MILLISECONDS);
+    }
+
+    private void stopButtonAction() {
+
+    }
+
+    private void quitButtonAction() {
+
+        System.exit(0);
+    }
 }
